85cad330c45dec98f01aeb7f287cb03d8f62c6c9
vincentRouget <vincent.rouget86@gmail.com>
2024-01-11 17:33:08 +0100

    Second commit

--- src/main/java/com/example/thymeleaf/controller/DoctorController.java
+++ src/main/java/com/example/thymeleaf/controller/DoctorController.java
@@ -1,6 +1,7 @@
 package com.example.thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class DoctorController {
 
     @GetMapping("/doctor/")
-    public String doctor(@RequestParam(required=false, defaultValue="0") int number,
-                         @RequestParam(required=false, defaultValue="John Smith") String name) {
-
+    public String doctor(
+            @RequestParam(required = false, defaultValue = "0") int number,
+            @RequestParam(required = false, defaultValue = "John Smith") String name,
+            Model model) {
+        model.addAttribute("doctorname", name);
+        model.addAttribute("doctornumber", number);
         return "doctor";
     }
 }
